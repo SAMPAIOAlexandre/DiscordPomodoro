46edf0f6ca0706953cb6b78cdd3f46ca074189ad
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-02-03 14:27:11 +0100

    feat: prettier installed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import 'dotenv/config';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 const token = process.env.DISCORD_TOKEN;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // The term "guild" is used by the Discord API and in discord.js to refer to a Discord server.
 
 client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 // Log in to Discord with your client's token
