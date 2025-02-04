2ac9534bfb1841c2d9e172801db68940a8278bd7
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-02-04 01:13:21 +0100

    feat: creating pomodoro bot files

---
+++ commands/pomodoro/start.js
@@ -0,0 +1,45 @@
+import { SlashCommandBuilder, ChannelType } from 'discord.js';
+
+export default {
+	data : new SlashCommandBuilder()
+		.setName('start')
+		.setDescription('Start a new pomodoro session with desired time.')
+		.addIntegerOption(option =>
+			option.setName('duration')
+				.setDescription('The time in minutes for the pomodoro session.')
+				.setRequired(true))
+		.addIntegerOption(option =>
+			option.setName('break')
+				.setDescription('Time of break in minutes.')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const duration = interaction.options.getInteger('duration');
+		const breakDuration = interaction.options.getInteger('break');
+		const guild = interaction.guild;
+		const category = guild.channels.cache.find(c => c.name === 'Sessions Pomodoro' && c.type === ChannelType.GuildCategory);
+
+		if (!category) {
+			await interaction.reply('The category "Sessions Pomodoro" does not exist. Please create it first.');
+			return;
+		}
+		const existingChannel = guild.channels.cache
+			.filter (channel => channel.name.startsWith(`Pomodoro ${duration}/${breakDuration}`))
+			.size;
+
+		const newChannelName = `Pomodoro ${duration}/${breakDuration} #${existingChannel + 1}`;
+
+		const newChannel = await guild.channels.create({
+			name : newChannelName,
+			type: ChannelType.GuildVoice,
+			parent: category.id,
+			permissionOverwrites: [
+				{
+					id: guild.roles.everyone.id,
+					deny: ['VIEW_CHANNEL'],
+				},
+			],
+		});
+		await interaction.reply(`Pomodoro session created in ${newChannel}`);
+	},
+};
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 
 const token = process.env.DISCORD_TOKEN;
 
-// Fix pour __dirname with ESM
+// Fix for __dirname with ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
